859a2980d6d4055ceb44e8ac845a897514a3227e
Clariza <clariza21@outlook.com>
2018-02-12 18:56:53 -0500

    Corazon Animado

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+  #app
+    Home
+</template>
+
+<script>
+import Home from './components/Home'
+export default {
+  components: {Home},
+  name: 'app',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  *{
+
+    font-family: 'Roboto', sans-serif;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Home from './components/Home'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+// new Vue({
+//   el: '#ejm',
+//   render: h => h(Home)
+// })
